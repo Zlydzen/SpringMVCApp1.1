b1ce901b52c0e253f7c8bc408948b0f107ad390c
Mikita Byshniou <nick.byshnev@gmail.com>
2021-12-12 11:56:13 +0300

    add hello.html goodbye.html pages

--- src/main/java/by/byshnev/springmvc/config/controllers/FirstController.java
+++ src/main/java/by/byshnev/springmvc/config/controllers/FirstController.java
@@ -1,10 +1,18 @@
 package by.byshnev.springmvc.config.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
+    @GetMapping("/hello")  //we want to get 'get-' requests
+    public String helloPage() {
+        return "first/hello";
+    }
 
-
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
 }
