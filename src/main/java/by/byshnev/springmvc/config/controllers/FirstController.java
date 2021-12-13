134323e1e96ced3e2fe1497942b5d12429766289
Mikita Byshniou <nick.byshnev@gmail.com>
2021-12-13 16:13:38 +0300

    my first Web calculator

--- src/main/java/by/byshnev/springmvc/config/controllers/FirstController.java
+++ src/main/java/by/byshnev/springmvc/config/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package by.byshnev.springmvc.config.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +12,36 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+//        System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
 
         return "first/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatior(@RequestParam(value = "a", required = false) Integer a,
+                              @RequestParam(value = "b", required = false) Integer b,
+                              @RequestParam(value = "action", required = false) String action,
+                              Model model) {
+
+        if (action.equals("multiplication")) {
+            action = String.valueOf(a * b);
+        }
+        if (action.equals("addition")) {
+            action = String.valueOf(a + b);
+        }
+        if (action.equals("subtraction")) {
+            action = String.valueOf(a - b);
+        }
+        if (action.equals("division")) {
+            action = String.valueOf(a / b);
+        }
+        model.addAttribute("calc", action);
+
+        return "first/calculator";
     }
 
     @GetMapping("/goodbye")
